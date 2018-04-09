363fafcf9b278dd40bd261aaf6c8e996eb4be3f5
Xinli Wang <xinli.wang@ibm.com>
2018-04-08 22:46:35 -0400

    Initial Commit

---
+++ EmotionPay/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  EmotionPay
+//
+//  Created by XinliWang on 4/8/18.
+//  Copyright © 2018 XinliWang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ EmotionPay/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  EmotionPay
+//
+//  Created by XinliWang on 4/8/18.
+//  Copyright © 2018 XinliWang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
